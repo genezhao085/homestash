1d0be1ad5ddad23a8348a359eeb3b9ec56c62167
Genezhao085 <genezhao085@gmail.com>
2026-07-22 15:24:43 +0800

    fix: web db init - use databaseFactoryFfiWeb setter with sqflite_common import

--- lib/utils/database_init_web.dart
+++ lib/utils/database_init_web.dart
@@ -1,5 +1,6 @@
+import 'package:sqflite_common/sqflite.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 Future<void> initDatabase() async {
-  databaseFactory = createDatabaseFactoryFfiWeb();
+  databaseFactory = databaseFactoryFfiWeb;
 }
